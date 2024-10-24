43cc9ffac689c1c889df7b6305b14c33f13b4dec
SaadSabah <saadmaseer@yahoo.com>
2024-10-25 00:14:38 +0200

    done with data form

--- main.js
+++ main.js
@@ -32,3 +32,8 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+window.onload = function() {
+    // Reset the form fields when the page loads
+    document.getElementById("form").reset();
+};
